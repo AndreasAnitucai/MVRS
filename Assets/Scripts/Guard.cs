6c9ff7b5e9fbee162d34a62a948bc5af082c1052
AndreasAnitucai <h22edan@axevalla.fhsk.se>
2024-03-20 11:53:19 +0100

    FoV Done

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -10,24 +10,11 @@ public class Guard : MonoBehaviour
 {
     public Transform pathHolder;
     public float speed = 5;
-    public float waitTime = .3f;
+    public float waitTime = 1f;
     public NavMeshAgent agent;
-
-    public float radius;
-    [Range(0, 360)]
-    public float angle;
-
-    public GameObject playerRef;
-
-    public LayerMask targetMask;
-    public LayerMask obstructionMask;
-
-    public bool canSeePlayer;
 
     private void Start()
     {
-        playerRef = GameObject.FindGameObjectWithTag("Player");
-        StartCoroutine(FOVRoutine());
 
         // Goes in a path
         Vector3[] waypoints = new Vector3[pathHolder.childCount];
@@ -39,40 +26,6 @@ public class Guard : MonoBehaviour
 
         StartCoroutine(followPath(waypoints)); 
     }
-
-
-    private IEnumerator FOVRoutine()
-    {
-        WaitForSeconds wait = new WaitForSeconds(.2f);
-
-
-        while(true)
-        {
-            yield return wait;
-            FieldOfViewCheck();
-        }
-    }
-    private void FieldOfViewCheck()
-    {
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
-
-        if(rangeChecks.Length != 0)
-        {
-            Transform target = rangeChecks[0].transform;
-            Vector3 diractionToTarget = (target.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, diractionToTarget) < angle / 2)
-            {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
-                if (!Physics.Raycast(transform.position, diractionToTarget, distanceToTarget, obstructionMask)) canSeePlayer = true;
-                else canSeePlayer = false;
-            }
-            else canSeePlayer = false;
-            
-        }
-        else if (canSeePlayer) canSeePlayer = false;
-    }
-
     IEnumerator followPath(Vector3[] waypoints)
     {
         transform.position = waypoints[0];
